963bc8f2181fa77ff7aed9066403467bcfb60e0b
Suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2023-03-23 21:41:02 +0100

    Initial commit

---
+++ modInfo.lua
@@ -0,0 +1,12 @@
+local modInfo = {
+    name = "MOD_NAME",
+    description = "MOD_DESCRIPTION",
+    preview = "preview.png",
+    version = "0.0.0",
+    type = "MOD_TYPE",
+    developer = "MOD_DEVELOPER",
+    website = "MOD_WEBSITE",
+}
+
+return modInfo 
+
